7575b3bd2edffb44370755041bd2d77e30b7ae4c
mciuchitu <xscrub96x@gmail.com>
2019-10-09 00:33:37 -0700

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DES_.Net_Core
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
